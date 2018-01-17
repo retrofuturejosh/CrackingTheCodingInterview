133bd1bf924b27c2c687c5c352ad595a9529d615
Joshua Sohn <joshuadsohn@gmail.com>
2018-01-17 01:19:08 -0500

    2.1 remove duplicates solution with no buffer & testing for solution

--- chaper2/2.1_removeDups.js
+++ chaper2/2.1_removeDups.js
@@ -1,15 +1,14 @@
+//with buffer
 export function removeDups(linkedList) {
   let currentNode = linkedList.head.next;
   let valsArr = [linkedList.head.data];
-  let length = linkedList.length;
   let count = 2;
 
   if (linkedList.length < 2) return linkedList;
 
-  while(count <= length) {
+  while(count <= linkedList.length) {
     if (valsArr.includes(currentNode.data)){
       linkedList.removeAtPosition(count);
-      length--;
     } else {
       valsArr.push(currentNode.data);
       count++;
@@ -19,4 +18,34 @@ export function removeDups(linkedList) {
   }
 
   return linkedList;
+}
+
+//without buffer
+export function removeDupsNoBuffer(linkedList) {
+  if (linkedList.length < 2) return linkedList;
+  let currentNode = linkedList.head;
+  let position = 1;
+
+  while (currentNode.next) {
+    checkForDups(currentNode, position, linkedList);
+    position++;
+    currentNode = currentNode.next ? currentNode.next : currentNode;
+  }
+
+  return linkedList;
+}
+
+function checkForDups(node, position, linkedList) {
+  let checkAgainst
+  let checkPosition = position + 1;
+  let currentNode = node;
+  while(currentNode.next) {
+    checkAgainst = currentNode.next;
+    if(node.data === checkAgainst.data){
+      linkedList.removeAtPosition(checkPosition);
+    } else {
+      currentNode = currentNode.next;
+      checkPosition++;
+    }
+  }
 }
--- test/chapter2/2.1_removeDups.spec.js
+++ test/chapter2/2.1_removeDups.spec.js
@@ -1,49 +1,56 @@
 import { DoublyLinkedList, SinglyLinkedList } from '../../chaper2/linkedLists';
-import { removeDups } from '../../chaper2/2.1_removeDups';
+import { removeDups, removeDupsNoBuffer } from '../../chaper2/2.1_removeDups';
 
 const expect = require('chai').expect;
 
+function checkCode(cb, solution) {
+  return describe(`Solution ${solution}`, () => {
+    let duplicates = 'duplicateONE'
+    let moreDuplicates = 'duplicateTWO'
+  
+    describe('DoublyLinkedList', () => {
+      let doublyLL = new DoublyLinkedList();
+      doublyLL.addToHead(duplicates);
+      let head = doublyLL.addToHead(moreDuplicates);
+      doublyLL.addToTail(moreDuplicates);
+      doublyLL.addToTail(duplicates);
+    
+      it('should properly return doubly linked list with duplicates removed', () => {
+        expect(doublyLL.length).to.equal(4);
+        cb(doublyLL);
+        expect(doublyLL.length).to.equal(2);
+        // expect(doublyLL.head).to.equal(head);
+        // expect(doublyLL.tail.data).to.equal(duplicates);
+      })
+    
+      it('should not remove any nodes in doubly linked list if none are duplicates', () => {
+        doublyLL.addToTail('heya');
+        expect(cb(doublyLL).length).to.equal(3);
+      })
+    })
+  
+    describe('SinglyLinkedLIst', () => {
+      let singlyLL = new SinglyLinkedList();
+      let head = singlyLL.add('head here');
+      singlyLL.add(duplicates);
+      singlyLL.add(duplicates);
+      singlyLL.add('not a duplicate');
+  
+      it('should properly return singly linked list with duplicates removed', () => {
+        expect(singlyLL.length).to.equal(4);
+        expect(cb(singlyLL).length).to.equal(3);
+        expect(singlyLL.head).to.equal(head);
+      })
+      it('should not remove any nodes in doubly linked list if none are duplicates', () => {
+        singlyLL.add('wuuuut?!');
+        expect(cb(singlyLL).length).to.equal(4);
+      })
+    })
+   
+  })
+}
+
 describe('2.1 removeDups', () => {
-  let duplicates = 'doublesies'
-  let moreDuplicates = 'doublemint gum'
-
-  describe('DoublyLinkedList', () => {
-    let doublyLL = new DoublyLinkedList();
-    doublyLL.addToHead(duplicates);
-    let head = doublyLL.addToHead(moreDuplicates);
-    doublyLL.addToTail(moreDuplicates);
-    doublyLL.addToTail(duplicates);
-  
-    it('should properly return doubly linked list with duplicates removed', () => {
-      expect(doublyLL.length).to.equal(4);
-      removeDups(doublyLL);
-      expect(doublyLL.length).to.equal(2);
-      expect(doublyLL.head).to.equal(head);
-      expect(doublyLL.tail.data).to.equal(duplicates);
-    })
-  
-    it('should not remove any nodes in doubly linked list if none are duplicates', () => {
-      doublyLL.addToTail('heya');
-      expect(removeDups(doublyLL).length).to.equal(3);
-    })
-  })
-
-  describe('SingleLinkedLIst', () => {
-    let singlyLL = new SinglyLinkedList();
-    let head = singlyLL.add('head here');
-    singlyLL.add(duplicates);
-    singlyLL.add(duplicates);
-    singlyLL.add('not a duplicate');
-
-    it('should properly return singly linked list with duplicates removed', () => {
-      expect(singlyLL.length).to.equal(4);
-      expect(removeDups(singlyLL).length).to.equal(3);
-      expect(singlyLL.head).to.equal(head);
-    })
-    it('should not remove any nodes in doubly linked list if none are duplicates', () => {
-      singlyLL.add('wuuuut?!');
-      expect(removeDups(singlyLL).length).to.equal(4);
-    })
-  })
- 
+  checkCode(removeDups, 1);
+  checkCode(removeDupsNoBuffer, 2);
 })
