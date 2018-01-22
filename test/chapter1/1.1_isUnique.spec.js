a7ed262329ef27ccfbc5d59bf55b085e88e551bf
Joshua Sohn <joshuadsohn@gmail.com>
2018-01-21 23:20:39 -0500

    cleans and modularizes test code for 1.1

--- test/chapter1/1.1_isUnique.spec.js
+++ test/chapter1/1.1_isUnique.spec.js
@@ -1,33 +1,26 @@
-const expect = require('chai').expect;
-
 import { isUnique, isUnique2, isUnique3 } from '../../chapter1/1.1_isUnique';
+const expect = require('chai').expect;
 
 //Testing for 1.1 isUnique
 describe('1.1 isUnique', () => {
-  describe('solution 1', () => {
-    testSolution1(isUnique);
-    testSolution2(isUnique);
-  });
-  describe('solution 2', () => {
-    testSolution1(isUnique2);
-    testSolution2(isUnique2);
-  });
-  describe('solution 3', () => {
-    testSolution1(isUnique3);
-    testSolution2(isUnique3);
-  });
+
+//ADD SOLUTIONS FOR TESTING BELOW
+  test('Solution 1', isUnique);
+  test('Solution 2', isUnique2);
+  test('Solution 3', isUnique3);
 });
 
-function testSolution1(cb) {
-  it('should return true if all characters are unique', () => {
-    expect(cb('abcdefg')).to.equal(true);
-    expect(cb('12345')).to.equal(true);
-  });
-}
+function test(description, cb) {
+  describe(description, () => {
 
-function testSolution2(cb) {
-  it('should return false if all characters are not unique', () => {
-    expect(cb('aba')).to.equal(false);
-    expect(cb('12334')).to.equal(false);
-  });
+    it('should return true if all characters are unique', () => {
+      expect(cb('abcdefg')).to.equal(true);
+      expect(cb('12345')).to.equal(true);
+    });
+
+    it('should return false if all characters are not unique', () => {
+      expect(cb('aba')).to.equal(false);
+      expect(cb('12334')).to.equal(false);
+    });
+  })
 }
