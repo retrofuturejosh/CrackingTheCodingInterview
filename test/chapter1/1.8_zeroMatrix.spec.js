e2c4f406490af2d0eb1d056a882b4167e298cb2e
Joshua Sohn <joshuadsohn@gmail.com>
2018-01-21 23:10:23 -0500

    more testing for 1.8 zero matrix

--- test/chapter1/1.8_zeroMatrix.spec.js
+++ test/chapter1/1.8_zeroMatrix.spec.js
@@ -20,8 +20,26 @@ function test(description, cb) {
       [5,  0, 0, 8],
       [0,  0, 0, 0],
       [13, 0, 0, 16]];
+
+    let matrix2 = 
+      [[0, 2],
+       [1, 2],
+       [1, 2],
+       [1, 2],
+       [1, 2],];
+
+    let matrix3 = [[1, 2, 3, 4, 0, 5, 6, 7], [1, 2, 3, 4, 5, 6, 7, 8]];
+
     it('properly sets entire row and column to zero', () => {
       expect(cb(matrix)).to.deep.equal(matrixSolution);
+      matrix2 = cb(matrix2);
+      expect(matrix2[0]).to.deep.equal([0, 0]);
+      expect(matrix2.length).to.equal(5);
+      expect(matrix2[4]).to.deep.equal([0, 2]);
+      matrix3 = cb(matrix3);
+      expect(matrix3[0]).to.deep.equal([0, 0, 0, 0, 0, 0, 0, 0]);
+      expect(matrix3[1]).to.deep.equal([1, 2, 3, 4, 0, 6, 7, 8]);
+      expect(matrix3.length).to.equal(2);
     })
   })
 }
