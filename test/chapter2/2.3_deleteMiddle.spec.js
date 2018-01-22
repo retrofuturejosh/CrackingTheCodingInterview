ba003d2365a196f9cf9f2025494fb179ceedadc1
Joshua Sohn <joshuadsohn@gmail.com>
2018-01-21 23:44:19 -0500

    modularizes testing for 2.3

--- test/chapter2/2.3_deleteMiddle.spec.js
+++ test/chapter2/2.3_deleteMiddle.spec.js
@@ -4,18 +4,25 @@ import { deleteMiddle } from '../../chaper2/2.3_deleteMiddle';
 const expect = require('chai').expect;
 
 describe('2.3 Delete Middle Node', () => {
-  let linkedList = new SinglyLinkedList();
-  addNodes(linkedList, 5);
-  let nodeToRemove = linkedList.searchAtPosition(2);
-  let nextNode = linkedList.searchAtPosition(3);
 
-  it('removes correct node', () => {
-    deleteMiddle(nodeToRemove);
-    expect(linkedList.searchAtPosition(2).data).to.equal(3);
-    expect(linkedList.searchAtPosition(2).next.data).to.equal(4);
-
-  })
+//ADD SOLUTIONS FOR TESTING BELOW
+  test('Solution 1', deleteMiddle);
 })
+
+function test(description, cb) {
+  describe(description, () => {
+    let linkedList = new SinglyLinkedList();
+    addNodes(linkedList, 5);
+    let nodeToRemove = linkedList.searchAtPosition(2);
+    let nextNode = linkedList.searchAtPosition(3);
+  
+    it('removes correct node', () => {
+      cb(nodeToRemove);
+      expect(linkedList.searchAtPosition(2).data).to.equal(3);
+      expect(linkedList.searchAtPosition(2).next.data).to.equal(4);
+    })
+  })
+}
 
 function addNodes(linkedList, num) {
   for(let i = 1; i <= num; i++) {
